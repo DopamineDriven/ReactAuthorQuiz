2bf6a29288e59924301d278814b45c4fd49443f7
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-17 06:47:24 -0500

    getTurnData function complete

--- authorquiz/src/index.js
+++ authorquiz/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import { shuffle, sample } from 'underscore';
 
 const authors = [
     {
@@ -45,18 +46,40 @@ const authors = [
     }
   ];
 
+// ------------ getTurnData OVERVIEW ------------
 // function selects set of possible answers 
 // join together list off books written by all authors in dataset
 // then shuffle them in a random order with underscore npm 
 // https://www.npmjs.com/package/underscore
 // then choosing the first four
+
 // build collection by reducing author collections and
 // concatenating each author's books into the larger set
 const getTurnData = (authors) => {
     const allBooks = authors.reduce((p, c, i) => {
         return p.concat(c.books)
     }, []);
-}
+    
+    // value fourRandomBooks equals shuffle all books
+    // use slice method to extract first four books returned
+    const fourRandomBooks = shuffle(allBooks).slice(0,4)
+    
+    // choose correct answer from fourRandomBooks
+    // use sample function to choose a random value
+    const answer = sample(fourRandomBooks)
+
+    // return authors via find method of authors array
+    // to find author such that authors book collection contains
+    // a books title is equal to the answer chosen
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => {
+            author.books.some((title) => {
+                title === answer
+            })
+        })
+    }
+};
 
 
 // props received by Turn will be author and books
