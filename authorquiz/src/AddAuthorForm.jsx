550661024bb729ceaf0c802580584a632a3e6bba
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-18 12:22:19 -0500

    splitting components, constructing

--- authorquiz/src/AddAuthorForm.jsx
+++ authorquiz/src/AddAuthorForm.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './AddAuthorForm.css';
+import './bootstrap.min.css'
+
+class AuthorForm extends Component {
+    constructor (props) {
+        // default state defined
+        super(props)
+        this.state = {
+            name: '',
+            imageURL: ''
+        }
+    }
+    // use value to bind inputs
+    render() {
+    return <form>
+        <div className="AddAuthorForm__input">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+        </div>
+        <div className="AddAuthorForm__input">
+            <label htmlFor="imageURL">Image URL</label>
+            <input type="text" name="imageURL" />
+        </div>
+    </form>
+    }
+}
+
 
 // adding form for users to add new authors/books/author image url to application
 // match is a prop supplied by router
@@ -7,12 +33,7 @@ import './AddAuthorForm.css';
 const AddAuthorForm = ({match}) => {
     return <div className="AddAuthorForm">
         <h1>Add Author</h1>
-            <form>
-                <div className="AddAuthorForm__input">
-                    <label htmlFor="name">Name</label>
-                    <input></input>
-                </div>
-            </form>
+        <AuthorForm />
     </div>
 }
 
