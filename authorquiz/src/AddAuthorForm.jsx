df4217c5268d2a1478be95e42f6fe97a8e6ea461
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-18 12:30:19 -0500

    methods, data-binding, onChange

--- authorquiz/src/AddAuthorForm.jsx
+++ authorquiz/src/AddAuthorForm.jsx
@@ -2,21 +2,33 @@ import React, { Component } from 'react';
 import './AddAuthorForm.css';
 import './bootstrap.min.css'
 
+// -----------------CONSTRUCTOR--------------------------------
 class AuthorForm extends Component {
     constructor (props) {
-        // default state defined
+        // default component state defined
         super(props)
         this.state = {
             name: '',
             imageURL: ''
         }
+        // -----------------BINDING--------------------------------
+        // since onFieldChange (method) uses this., data-binding event in constructor required
+        this.onFieldChange = this.onFieldChange.bind(this);
     }
+    // -----------------METHODS--------------------------------
+    // use onFieldChange to modify default component state
+    onFieldChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })    
+    }
+        // -----------------RENDER--------------------------------
     // use value to bind inputs (name, imageURL)
     render() {
     return <form>
         <div className="AddAuthorForm__input">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" value={this.state.name}/>
+            <input type="text" name="name" value={this.state.name} onChange={this.onFieldChange} />
         </div>
         <div className="AddAuthorForm__input">
             <label htmlFor="imageURL">Image URL</label>
