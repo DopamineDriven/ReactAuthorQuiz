3120778842d8fb1da8c394e851efa73d0e6cc166
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-18 11:50:58 -0500

    Link /add

--- authorquiz/src/AuthorQuiz.js
+++ authorquiz/src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import './App.css';
 import './bootstrap.min.css'
@@ -98,6 +99,7 @@ const AuthorQuiz = ({ turnData, highlight, onAnswerSelected }) => {
       <Hero />
       <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
       <Continue />
+      <p><Link to="/add">Add an author</Link></p>
       <Footer />
     </div>
   )
